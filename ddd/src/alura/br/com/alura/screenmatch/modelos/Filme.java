a6616163c2cff994d68f2d150225f1e1c50e9bc4
jonasdasneves <jonas.joliveira2024@gmail.com>
2024-12-15 22:15:32 -0300

    alura - formacao Java - curso 2 - poo - modulo 3

--- ddd/src/alura/br/com/alura/screenmatch/modelos/Filme.java
+++ ddd/src/alura/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,73 +1,14 @@
 package alura.br.com.alura.screenmatch.modelos;
 
-public class Filme{
+public class Filme extends Titulo{
 
-    private String nome;
-    private int anoDelançamento;
-    private boolean incluidoNoPlano;
-    private double somaDasAvaliacoes;
-    private int totaldeAvaliacoes;
-    private int duracaoEmMinutos;
+    private String diretor;
 
-    public void exibeFichaTecnica(){
-        System.out.println(nome);
-        System.out.println(anoDelançamento);
+    public String getDiretor() {
+        return diretor;
     }
 
-    public void avalia(double nota){
-        somaDasAvaliacoes += nota;
-        totaldeAvaliacoes ++;
-    }
-
-    public double pegaMedia(){
-        return (somaDasAvaliacoes/totaldeAvaliacoes);
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public int getAnoDelançamento() {
-        return anoDelançamento;
-    }
-
-    public void setAnoDelançamento(int anoDelançamento) {
-        this.anoDelançamento = anoDelançamento;
-    }
-
-    public boolean isIncluidoNoPlano() {
-        return incluidoNoPlano;
-    }
-
-    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
-        this.incluidoNoPlano = incluidoNoPlano;
-    }
-
-    public double getSomaDasAvaliacoes() {
-        return somaDasAvaliacoes;
-    }
-
-    public void setSomaDasAvaliacoes(double somaDasAvaliacoes) {
-        this.somaDasAvaliacoes = somaDasAvaliacoes;
-    }
-
-    public int getTotaldeAvaliacoes() {
-        return totaldeAvaliacoes;
-    }
-
-    public void setTotaldeAvaliacoes(int totaldeAvaliacoes) {
-        this.totaldeAvaliacoes = totaldeAvaliacoes;
-    }
-
-    public int getDuracaoEmMinutos() {
-        return duracaoEmMinutos;
-    }
-
-    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
-        this.duracaoEmMinutos = duracaoEmMinutos;
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
     }
 }
--- ddd/src/alura/br/com/alura/screenmatch/modelos/Principal.java
+++ ddd/src/alura/br/com/alura/screenmatch/modelos/Principal.java
@@ -1,5 +1,7 @@
 package alura.br.com.alura.screenmatch.modelos;
 
+import alura.br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+
 public class Principal {
 
     public static void main(String[] args) {
@@ -7,11 +9,26 @@ public class Principal {
         meuFilme.setNome("O poderoso chefão");
         meuFilme.setDuracaoEmMinutos(120);
         meuFilme.setAnoDelançamento(1988);
+        System.out.println(meuFilme.getDuracaoEmMinutos());
 
         meuFilme.exibeFichaTecnica();
         meuFilme.avalia(5);
         meuFilme.avalia(10);
         meuFilme.avalia(8);
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDelançamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporada(12);
+        lost.setMinutosPorEpisodio(45);
+        System.out.println("Minutos para maratonar lost: "+lost.getDuracaoEmMinutos());
+
+        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        calculadora.inclui(meuFilme);
+        calculadora.inclui(lost);
+        System.out.println(calculadora.getTempoTotal());
     }
 }
